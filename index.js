7ab305abfb5e24c6a264a34a7de7fc446bccc40d
ramakrishnapakala <ramakrishnapakala25@gmail.com>
2025-12-09 22:57:46 +0530

    done second

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ console.log(name);
 console.log(10 + 5);
 const a = 10;
 console.log(a);
+
+const greetings = (name) => {
+  console.log(`welcome ${name}`);
+};
+greetings("kittu");
---
+++ script.py
@@ -0,0 +1,4 @@
+def greetings(name):
+     print(f"welcome {name}")
+greetings("rk")
+print(5+10)
